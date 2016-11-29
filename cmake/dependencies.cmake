a0c6a8b7d3eb20b44a42c3d8563c56086e0eeab0
ranjak <ranjak.hector@gmail.com>
2016-11-29 17:33:23 +0100

    CMake: add the ability to automatically download/update glm.
    Also add automatic submodule init/update for glad.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,13 +1,53 @@
 # OpenGL app dependencies
 
-find_package(glm REQUIRED)
 find_package(glfw3 REQUIRED)
+
+# Use glm for 3D math functionality
+
+option(OGLTUTOR_SYSTEM_GLM "Use the system-provided glm library." ON)
+
+if (OGLTUTOR_SYSTEM_GLM)
+
+  find_package(glm REQUIRED)
+
+else(OGLTUTOR_SYSTEM_GLM)
+
+  # Grab glm from its git repository
+  find_program(GIT git)
+  if (NOT GIT)
+    message(FATAL_ERROR "Git is required to download glm, but was not found on the system.")
+  endif()
+
+  if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/glm")
+
+    execute_process(COMMAND "${GIT}" clone "https://github.com/g-truc/glm"
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty"
+    )
+  else()
+    execute_process(COMMAND "${GIT}" pull
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/glm"
+    )
+  endif()
+
+  add_subdirectory("3rdparty/glm" ${CMAKE_CURRENT_BINARY_DIR}/glm)
+
+endif(OGLTUTOR_SYSTEM_GLM)
+
 
 # Use glad for OpenGL extension loading
 
-option(OGLTUTOR_BUNDLED_GLAD "Use the bundled Glad OpenGL extension loader. Set to OFF if Glad is already installed on your system." ON)
+option(OGLTUTOR_SYSTEM_GLAD "Set to ON if the Glad OpenGL extension loader is installed on your system. Set to OFF to use the bundled Glad." OFF)
 
-if (OGLTUTOR_BUNDLED_GLAD)
+if (NOT OGLTUTOR_SYSTEM_GLAD)
+
+  find_program(GIT git)
+  if (NOT GIT)
+    message(FATAL_ERROR "Git is required to download glad, but was not found on the system.")
+  endif()
+
+  execute_process(COMMAND "${GIT}" submodule update --init -- 3rdparty/glad
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+  )
 
   set(GLAD_PROFILE "compatibility" CACHE INTERNAL "OpenGL profile" FORCE)
   set(GLAD_API "gl=3.3" CACHE INTERNAL "API type/version pairs, like \"gl=3.2,gles=\", no version means latest" FORCE)
@@ -18,8 +58,8 @@ if (OGLTUTOR_BUNDLED_GLAD)
 
   add_subdirectory("3rdparty/glad" ${CMAKE_CURRENT_BINARY_DIR}/glad)
 
-else(OGLTUTOR_BUNDLED_GLAD)
+else(NOT OGLTUTOR_SYSTEM_GLAD)
 
   message(FATAL_ERROR "Using system Glad is not supported yet.")
 
-endif(OGLTUTOR_BUNDLED_GLAD)
+endif(NOT OGLTUTOR_SYSTEM_GLAD)
