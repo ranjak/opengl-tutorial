5d20096b8892bade7b8dfb13f249610659fda000
ranjak <ranjak.hector@gmail.com>
2017-01-04 14:52:55 +0100

    World Scene tutorial: directly use an object-to-camera matrix.

--- src/system/mainloop.cpp
+++ src/system/mainloop.cpp
@@ -1,7 +1,7 @@
 #include "mainloop.hpp"
 #include "log.hpp"
 #include "system.hpp"
-#include "world/worldsceneubo.hpp"
+#include "world/worldsceneobjtocam.hpp"
 #include "glfw/windowglfw.hpp"
 #include <algorithm>
 
@@ -52,7 +52,7 @@ bool MainLoop::init(int width, int height, const std::string& title)
   mMainWindow->setCloseCallback([](Window*) { MainLoop::requestExit(); });
   mMainWindow->setSwapInterval(1);
 
-  mTutorial.reset(new WorldSceneUBO(mMainWindow.get()));
+  mTutorial.reset(new WorldSceneObjToCam(mMainWindow.get()));
   mTutorial->init();
 
   return true;
