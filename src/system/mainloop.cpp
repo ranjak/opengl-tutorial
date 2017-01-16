2b19abcdca81a66b74290c55c5b8f6f34f8528ce
ranjak <ranjak.hector@gmail.com>
2017-01-16 15:57:33 +0100

    Add Light Attenuation tutorial. Press H to toggle between inverse and inverse squared attenuation. Press O/U to tweak attenuation factor.

--- src/system/mainloop.cpp
+++ src/system/mainloop.cpp
@@ -1,7 +1,7 @@
 #include "mainloop.hpp"
 #include "log.hpp"
 #include "system.hpp"
-#include "illumination/fragmentpointlighting.hpp"
+#include "illumination/fragmentattenuation.hpp"
 #include "glfw/windowglfw.hpp"
 #include <algorithm>
 
@@ -47,7 +47,7 @@ bool MainLoop::init(int width, int height, const std::string& title)
   mMainWindow->setCloseCallback([](Window*) { MainLoop::requestExit(); });
   mMainWindow->setSwapInterval(1);
 
-  mTutorial.reset(new FragmentPointLighting(mMainWindow.get()));
+  mTutorial.reset(new FragmentAttenuation(mMainWindow.get()));
   mTutorial->init();
 
   return true;
